15df67744008f88b0fd94cbe19becaed7bf88495
Azhan Ali <azhanali2304@gmail.com>
2026-04-26 08:20:10 +0530

    changing minnor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} h-full`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col light-theme">{children}</body>
     </html>
   );
 }
